206137181e054015a482105f1465008294e99c2b
user1 <user1@user1-PC>
2019-08-01 19:37:42 +0900

    방금수정

--- src/member/model/vo/Member.java
+++ src/member/model/vo/Member.java
@@ -9,16 +9,15 @@ public class Member implements java.io.Serializable { /**
 	private static final long serialVersionUID = 6681603411878778107L; // 직렬화 해서 외부로 보낼 때 구분을 위해 ID를 지정해주어 버전관리를 해주어야 함.
 	
 	private int userNo;
-	private String userId;
+	private String userEmail;
 	private String userPwd;
 	private String userName;
 	private String phone;
-	private String email;
-	private String address;
-	private String interest;
 	private Date enrollDate;
 	private Date modifyDate;
-	private String status;
+	private String userEvent;
+	private String userStatus;
+	private String userMno;
 	
 	public Member() {
 		// TODO Auto-generated constructor stub
@@ -28,148 +27,139 @@ public class Member implements java.io.Serializable { /**
 	// 로그인 용 생성자
 	public Member(String userId, String userPwd) {
 		super();
-		this.userId = userId;
+		this.userEmail = userId;
 		this.userPwd = userPwd;
 	}
 
-	
 
-	public Member(String userId, String userName, String phone, String email, String address, String interest) {
+	public Member(int userNo, String userEmail, String userPwd, String userName, String phone, Date enrollDate,
+			Date modifyDate, String userEvent, String userStatus, String userMno) {
 		super();
-		this.userId = userId;
-		this.userName = userName;
-		this.phone = phone;
-		this.email = email;
-		this.address = address;
-		this.interest = interest;
-	}
-
-	
-	
-
-	public Member(String userId, String userPwd, String userName, String phone, String email, String address,
-			String interest) {
-		super();
-		this.userId = userId;
-		this.userPwd = userPwd;
-		this.userName = userName;
-		this.phone = phone;
-		this.email = email;
-		this.address = address;
-		this.interest = interest;
-	}
-
-
-	public Member(int userNo, String userId, String userPwd, String userName, String phone, String email,
-			String address, String interest, Date enrollDate, Date modifyDate, String status) {
-		
 		this.userNo = userNo;
-		this.userId = userId;
+		this.userEmail = userEmail;
 		this.userPwd = userPwd;
 		this.userName = userName;
 		this.phone = phone;
-		this.email = email;
-		this.address = address;
-		this.interest = interest;
 		this.enrollDate = enrollDate;
 		this.modifyDate = modifyDate;
-		this.status = status;
+		this.userEvent = userEvent;
+		this.userStatus = userStatus;
+		this.userMno = userMno;
 	}
+
 
 	public int getUserNo() {
 		return userNo;
 	}
 
+
 	public void setUserNo(int userNo) {
 		this.userNo = userNo;
 	}
 
-	public String getUserId() {
-		return userId;
+
+	public String getUserEmail() {
+		return userEmail;
 	}
 
-	public void setUserId(String userId) {
-		this.userId = userId;
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
 	}
+
 
 	public String getUserPwd() {
 		return userPwd;
 	}
 
+
 	public void setUserPwd(String userPwd) {
 		this.userPwd = userPwd;
 	}
+
 
 	public String getUserName() {
 		return userName;
 	}
 
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 
 	public String getPhone() {
 		return phone;
 	}
 
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getInterest() {
-		return interest;
-	}
-
-	public void setInterest(String interest) {
-		this.interest = interest;
-	}
 
 	public Date getEnrollDate() {
 		return enrollDate;
 	}
 
+
 	public void setEnrollDate(Date enrollDate) {
 		this.enrollDate = enrollDate;
 	}
+
 
 	public Date getModifyDate() {
 		return modifyDate;
 	}
 
+
 	public void setModifyDate(Date modifyDate) {
 		this.modifyDate = modifyDate;
 	}
 
-	public String getStatus() {
-		return status;
+
+	public String getUserEvent() {
+		return userEvent;
 	}
 
-	public void setStatus(String status) {
-		this.status = status;
+
+	public void setUserEvent(String userEvent) {
+		this.userEvent = userEvent;
 	}
+
+
+	public String getUserStatus() {
+		return userStatus;
+	}
+
+
+	public void setUserStatus(String userStatus) {
+		this.userStatus = userStatus;
+	}
+
+
+	public String getUserMno() {
+		return userMno;
+	}
+
+
+	public void setUserMno(String userMno) {
+		this.userMno = userMno;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 
 	@Override
 	public String toString() {
-		return "Member [userNo=" + userNo + ", userId=" + userId + ", userPwd=" + userPwd + ", userName=" + userName
-				+ ", phone=" + phone + ", email=" + email + ", address=" + address + ", interest=" + interest
-				+ ", enrollDate=" + enrollDate + ", modifyDate=" + modifyDate + ", status=" + status + "]";
+		return "Member [userNo=" + userNo + ", userEmail=" + userEmail + ", userPwd=" + userPwd + ", userName="
+				+ userName + ", phone=" + phone + ", enrollDate=" + enrollDate + ", modifyDate=" + modifyDate
+				+ ", userEvent=" + userEvent + ", userStatus=" + userStatus + ", userMno=" + userMno + "]";
 	}
+
 	
 	
 }
