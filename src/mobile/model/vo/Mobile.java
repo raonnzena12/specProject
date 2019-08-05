150c60ddd98d385e81c41692c8a99abf1b7adbf0
Administrator <Administrator@OUROBOROS>
2019-08-05 23:53:18 +0900

    아이고 학원가서 해야지

---
+++ src/mobile/model/vo/Mobile.java
@@ -0,0 +1,544 @@
+package mobile.model.vo;
+
+import java.sql.Date;
+
+public class Mobile {
+	private int mNo;
+	private String mCode;
+	private String mName;
+	private String mNameEn;
+	private String mBrandName;
+	private Date mReleaseDate;
+	private String mOsName;
+	private String mOsVersion;
+	private String mMaterial;
+	private String mSize;
+	private String mWeight;
+	private String mLink;
+	private String mInch;
+	private String mResolution;
+	private String mPixel;
+	private String mDisplayType;
+	private String mDisWidth;
+	private String mDisHeight;
+	private String mAp;
+	private String mCpu;
+	private String mCpuCore;
+	private String mCpuClock;
+	private String mGpu;
+	private String mRam;
+	private String mInnerMemory;
+	private String mOuterMemory;
+	private String mSensor;
+	private String mIris;
+	private String mFlash;
+	private String mPicResolution;
+	private String mVidResolution;
+	private String mVidFrame;
+	private String mFrontResolution;
+	private String mFrontVidResolution;
+	private String mFrontVidFrame;
+	private String mCamera;
+	private int mBattery;
+	private String mBatteryType;
+	private String mFastCharsing;
+	private String mRemovableBat;
+	private String mWireless;
+	private String mStandBy;
+	private String mProtocol;
+	private String mWifi;
+	private String mBluetooth;
+	private String mUsb;
+	private String mBio;
+	private String mPayment;
+	private String mVerify;
+	private String mEtc;
+	private String mStatus;
+	
+	public Mobile() {}
+
+	public Mobile(int mNo, String mCode, String mName, String mNameEn, String mBrandName, Date mReleaseDate,
+			String mOsName, String mOsVersion, String mMaterial, String mSize, String mWeight, String mLink,
+			String mInch, String mResolution, String mPixel, String mDisplayType, String mDisWidth, String mDisHeight,
+			String mAp, String mCpu, String mCpuCore, String mCpuClock, String mGpu, String mRam, String mInnerMemory,
+			String mOuterMemory, String mSensor, String mIris, String mFlash, String mPicResolution,
+			String mVidResolution, String mVidFrame, String mFrontResolution, String mFrontVidResolution,
+			String mFrontVidFrame, String mCamera, int mBattery, String mBatteryType, String mFastCharsing,
+			String mRemovableBat, String mWireless, String mStandBy, String mProtocol, String mWifi, String mBluetooth,
+			String mUsb, String mBio, String mPayment, String mVerify, String mEtc, String mStatus) {
+		super();
+		this.mNo = mNo;
+		this.mCode = mCode;
+		this.mName = mName;
+		this.mNameEn = mNameEn;
+		this.mBrandName = mBrandName;
+		this.mReleaseDate = mReleaseDate;
+		this.mOsName = mOsName;
+		this.mOsVersion = mOsVersion;
+		this.mMaterial = mMaterial;
+		this.mSize = mSize;
+		this.mWeight = mWeight;
+		this.mLink = mLink;
+		this.mInch = mInch;
+		this.mResolution = mResolution;
+		this.mPixel = mPixel;
+		this.mDisplayType = mDisplayType;
+		this.mDisWidth = mDisWidth;
+		this.mDisHeight = mDisHeight;
+		this.mAp = mAp;
+		this.mCpu = mCpu;
+		this.mCpuCore = mCpuCore;
+		this.mCpuClock = mCpuClock;
+		this.mGpu = mGpu;
+		this.mRam = mRam;
+		this.mInnerMemory = mInnerMemory;
+		this.mOuterMemory = mOuterMemory;
+		this.mSensor = mSensor;
+		this.mIris = mIris;
+		this.mFlash = mFlash;
+		this.mPicResolution = mPicResolution;
+		this.mVidResolution = mVidResolution;
+		this.mVidFrame = mVidFrame;
+		this.mFrontResolution = mFrontResolution;
+		this.mFrontVidResolution = mFrontVidResolution;
+		this.mFrontVidFrame = mFrontVidFrame;
+		this.mCamera = mCamera;
+		this.mBattery = mBattery;
+		this.mBatteryType = mBatteryType;
+		this.mFastCharsing = mFastCharsing;
+		this.mRemovableBat = mRemovableBat;
+		this.mWireless = mWireless;
+		this.mStandBy = mStandBy;
+		this.mProtocol = mProtocol;
+		this.mWifi = mWifi;
+		this.mBluetooth = mBluetooth;
+		this.mUsb = mUsb;
+		this.mBio = mBio;
+		this.mPayment = mPayment;
+		this.mVerify = mVerify;
+		this.mEtc = mEtc;
+		this.mStatus = mStatus;
+	}
+
+	public int getmNo() {
+		return mNo;
+	}
+
+	public void setmNo(int mNo) {
+		this.mNo = mNo;
+	}
+
+	public String getmCode() {
+		return mCode;
+	}
+
+	public void setmCode(String mCode) {
+		this.mCode = mCode;
+	}
+
+	public String getmName() {
+		return mName;
+	}
+
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+
+	public String getmNameEn() {
+		return mNameEn;
+	}
+
+	public void setmNameEn(String mNameEn) {
+		this.mNameEn = mNameEn;
+	}
+
+	public String getmBrandName() {
+		return mBrandName;
+	}
+
+	public void setmBrandName(String mBrandName) {
+		this.mBrandName = mBrandName;
+	}
+
+	public Date getmReleaseDate() {
+		return mReleaseDate;
+	}
+
+	public void setmReleaseDate(Date mReleaseDate) {
+		this.mReleaseDate = mReleaseDate;
+	}
+
+	public String getmOsName() {
+		return mOsName;
+	}
+
+	public void setmOsName(String mOsName) {
+		this.mOsName = mOsName;
+	}
+
+	public String getmOsVersion() {
+		return mOsVersion;
+	}
+
+	public void setmOsVersion(String mOsVersion) {
+		this.mOsVersion = mOsVersion;
+	}
+
+	public String getmMaterial() {
+		return mMaterial;
+	}
+
+	public void setmMaterial(String mMaterial) {
+		this.mMaterial = mMaterial;
+	}
+
+	public String getmSize() {
+		return mSize;
+	}
+
+	public void setmSize(String mSize) {
+		this.mSize = mSize;
+	}
+
+	public String getmWeight() {
+		return mWeight;
+	}
+
+	public void setmWeight(String mWeight) {
+		this.mWeight = mWeight;
+	}
+
+	public String getmLink() {
+		return mLink;
+	}
+
+	public void setmLink(String mLink) {
+		this.mLink = mLink;
+	}
+
+	public String getmInch() {
+		return mInch;
+	}
+
+	public void setmInch(String mInch) {
+		this.mInch = mInch;
+	}
+
+	public String getmResolution() {
+		return mResolution;
+	}
+
+	public void setmResolution(String mResolution) {
+		this.mResolution = mResolution;
+	}
+
+	public String getmPixel() {
+		return mPixel;
+	}
+
+	public void setmPixel(String mPixel) {
+		this.mPixel = mPixel;
+	}
+
+	public String getmDisplayType() {
+		return mDisplayType;
+	}
+
+	public void setmDisplayType(String mDisplayType) {
+		this.mDisplayType = mDisplayType;
+	}
+
+	public String getmDisWidth() {
+		return mDisWidth;
+	}
+
+	public void setmDisWidth(String mDisWidth) {
+		this.mDisWidth = mDisWidth;
+	}
+
+	public String getmDisHeight() {
+		return mDisHeight;
+	}
+
+	public void setmDisHeight(String mDisHeight) {
+		this.mDisHeight = mDisHeight;
+	}
+
+	public String getmAp() {
+		return mAp;
+	}
+
+	public void setmAp(String mAp) {
+		this.mAp = mAp;
+	}
+
+	public String getmCpu() {
+		return mCpu;
+	}
+
+	public void setmCpu(String mCpu) {
+		this.mCpu = mCpu;
+	}
+
+	public String getmCpuCore() {
+		return mCpuCore;
+	}
+
+	public void setmCpuCore(String mCpuCore) {
+		this.mCpuCore = mCpuCore;
+	}
+
+	public String getmCpuClock() {
+		return mCpuClock;
+	}
+
+	public void setmCpuClock(String mCpuClock) {
+		this.mCpuClock = mCpuClock;
+	}
+
+	public String getmGpu() {
+		return mGpu;
+	}
+
+	public void setmGpu(String mGpu) {
+		this.mGpu = mGpu;
+	}
+
+	public String getmRam() {
+		return mRam;
+	}
+
+	public void setmRam(String mRam) {
+		this.mRam = mRam;
+	}
+
+	public String getmInnerMemory() {
+		return mInnerMemory;
+	}
+
+	public void setmInnerMemory(String mInnerMemory) {
+		this.mInnerMemory = mInnerMemory;
+	}
+
+	public String getmOuterMemory() {
+		return mOuterMemory;
+	}
+
+	public void setmOuterMemory(String mOuterMemory) {
+		this.mOuterMemory = mOuterMemory;
+	}
+
+	public String getmSensor() {
+		return mSensor;
+	}
+
+	public void setmSensor(String mSensor) {
+		this.mSensor = mSensor;
+	}
+
+	public String getmIris() {
+		return mIris;
+	}
+
+	public void setmIris(String mIris) {
+		this.mIris = mIris;
+	}
+
+	public String getmFlash() {
+		return mFlash;
+	}
+
+	public void setmFlash(String mFlash) {
+		this.mFlash = mFlash;
+	}
+
+	public String getmPicResolution() {
+		return mPicResolution;
+	}
+
+	public void setmPicResolution(String mPicResolution) {
+		this.mPicResolution = mPicResolution;
+	}
+
+	public String getmVidResolution() {
+		return mVidResolution;
+	}
+
+	public void setmVidResolution(String mVidResolution) {
+		this.mVidResolution = mVidResolution;
+	}
+
+	public String getmVidFrame() {
+		return mVidFrame;
+	}
+
+	public void setmVidFrame(String mVidFrame) {
+		this.mVidFrame = mVidFrame;
+	}
+
+	public String getmFrontResolution() {
+		return mFrontResolution;
+	}
+
+	public void setmFrontResolution(String mFrontResolution) {
+		this.mFrontResolution = mFrontResolution;
+	}
+
+	public String getmFrontVidResolution() {
+		return mFrontVidResolution;
+	}
+
+	public void setmFrontVidResolution(String mFrontVidResolution) {
+		this.mFrontVidResolution = mFrontVidResolution;
+	}
+
+	public String getmFrontVidFrame() {
+		return mFrontVidFrame;
+	}
+
+	public void setmFrontVidFrame(String mFrontVidFrame) {
+		this.mFrontVidFrame = mFrontVidFrame;
+	}
+
+	public String getmCamera() {
+		return mCamera;
+	}
+
+	public void setmCamera(String mCamera) {
+		this.mCamera = mCamera;
+	}
+
+	public int getmBattery() {
+		return mBattery;
+	}
+
+	public void setmBattery(int mBattery) {
+		this.mBattery = mBattery;
+	}
+
+	public String getmBatteryType() {
+		return mBatteryType;
+	}
+
+	public void setmBatteryType(String mBatteryType) {
+		this.mBatteryType = mBatteryType;
+	}
+
+	public String getmFastCharsing() {
+		return mFastCharsing;
+	}
+
+	public void setmFastCharsing(String mFastCharsing) {
+		this.mFastCharsing = mFastCharsing;
+	}
+
+	public String getmRemovableBat() {
+		return mRemovableBat;
+	}
+
+	public void setmRemovableBat(String mRemovableBat) {
+		this.mRemovableBat = mRemovableBat;
+	}
+
+	public String getmWireless() {
+		return mWireless;
+	}
+
+	public void setmWireless(String mWireless) {
+		this.mWireless = mWireless;
+	}
+
+	public String getmStandBy() {
+		return mStandBy;
+	}
+
+	public void setmStandBy(String mStandBy) {
+		this.mStandBy = mStandBy;
+	}
+
+	public String getmProtocol() {
+		return mProtocol;
+	}
+
+	public void setmProtocol(String mProtocol) {
+		this.mProtocol = mProtocol;
+	}
+
+	public String getmWifi() {
+		return mWifi;
+	}
+
+	public void setmWifi(String mWifi) {
+		this.mWifi = mWifi;
+	}
+
+	public String getmBluetooth() {
+		return mBluetooth;
+	}
+
+	public void setmBluetooth(String mBluetooth) {
+		this.mBluetooth = mBluetooth;
+	}
+
+	public String getmUsb() {
+		return mUsb;
+	}
+
+	public void setmUsb(String mUsb) {
+		this.mUsb = mUsb;
+	}
+
+	public String getmBio() {
+		return mBio;
+	}
+
+	public void setmBio(String mBio) {
+		this.mBio = mBio;
+	}
+
+	public String getmPayment() {
+		return mPayment;
+	}
+
+	public void setmPayment(String mPayment) {
+		this.mPayment = mPayment;
+	}
+
+	public String getmVerify() {
+		return mVerify;
+	}
+
+	public void setmVerify(String mVerify) {
+		this.mVerify = mVerify;
+	}
+
+	public String getmEtc() {
+		return mEtc;
+	}
+
+	public void setmEtc(String mEtc) {
+		this.mEtc = mEtc;
+	}
+
+	public String getmStatus() {
+		return mStatus;
+	}
+
+	public void setmStatus(String mStatus) {
+		this.mStatus = mStatus;
+	}
+
+	@Override
+	public String toString() {
+		return mNo + " / " + mCode + " / " + mName + " / " + mNameEn + " / " + mBrandName + " / " + mReleaseDate + " / "
+				+ mOsName + " / " + mOsVersion + " / " + mMaterial + " / " + mSize + " / " + mWeight + " / " + mLink
+				+ " / " + mInch + " / " + mResolution + " / " + mPixel + " / " + mDisplayType + " / " + mDisWidth
+				+ " / " + mDisHeight + " / " + mAp + " / " + mCpu + " / " + mCpuCore + " / " + mCpuClock + " / " + mGpu
+				+ " / " + mRam + " / " + mInnerMemory + " / " + mOuterMemory + " / " + mSensor + " / " + mIris + " / "
+				+ mFlash + " / " + mPicResolution + " / " + mVidResolution + " / " + mVidFrame + " / "
+				+ mFrontResolution + " / " + mFrontVidResolution + " / " + mFrontVidFrame + " / " + mCamera + " / "
+				+ mBattery + " / " + mBatteryType + " / " + mFastCharsing + " / " + mRemovableBat + " / " + mWireless
+				+ " / " + mStandBy + " / " + mProtocol + " / " + mWifi + " / " + mBluetooth + " / " + mUsb + " / "
+				+ mBio + " / " + mPayment + " / " + mVerify + " / " + mEtc + " / " + mStatus;
+	}
+}
